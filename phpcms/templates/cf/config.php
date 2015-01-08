4c00cb2267260447163b4f34c4d40c5de05230ad
LegendHB <legendhb#gmail.com>
2015-01-08 11:06:37 +0800

    initialize
    Initialize the phpcms frameword.

---
+++ phpcms/templates/cf/config.php
@@ -0,0 +1,18 @@
+<?php return array (
+  'name' => '本站定制模版',
+  'disable' => 0,
+  'dirname' => 'cf',
+  'file_explan' => 
+  array (
+    'templates|cf|' => 
+    array (
+      'content' => '',
+    ),
+    'templates|cf|content' => 
+    array (
+      'category.html' => '文章频道',
+      'list.html' => '文章列表',
+      'show.html' => '文章内容',
+    ),
+  ),
+);?>
